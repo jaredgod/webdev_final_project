849a56e5b48766e9ef5a3963962c60b186a8304b
Jared Goddard <goddajar@flip2.engr.oregonstate.edu>
2020-12-06 14:59:15 -0800

    prototype

---
+++ highscore_save/highscore.js
@@ -0,0 +1,31 @@
+
+/*
+returns an array of objects with a "highscore", "name", and "rank"
+*/
+function getHighScoreList(){
+  var obj = {
+    rank: 0,
+    name: "",
+    highscore: ""
+  }
+  return [obj];
+}
+
+/*
+will save a highscore to the highscore.json file.
+*/
+function addHighScore(name, score, pass=""){
+  return;
+}
+
+/*
+returns a single object with a "highscore", "name", and "rank" based on the name
+*/
+function getHighScore(name){
+  var obj = {
+    rank: 0,
+    name: "",
+    highscore: ""
+  }
+  return obj;
+}
