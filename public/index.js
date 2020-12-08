6d4a1c6b5f7efb3348439f4d3996bf5ad285ff57
Jared Goddard <goddajar@flip2.engr.oregonstate.edu>
2020-12-07 20:55:48 -0800

    highscore update

--- public/index.js
+++ public/index.js
@@ -1,1 +1,21 @@
+//will save a highscore to the highscore.json file.
+//password is optional, no password is "".
+function addHighScore(name, score, pass=""){
 
+  var data = {
+    name: name,
+    score: score,
+    pass: pass
+  }
+
+  fetch("/addHighScore", {
+          method: 'PUT',
+          body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(function(response) {
+    if (!response.ok) {
+      console.log("Could not add high score.");
+    }
+  });
+}
